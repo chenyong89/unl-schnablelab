53702a44bf7abb8de310179528dec5a9790b8e6b
Chenyong Miao <cmiao@Chenyongs-iMac.search.charter.net>
2024-04-07 22:54:19 -0500

    flake8 and fix bugs

--- src/schnablelab/autoGWAS/gwas.py
+++ src/schnablelab/autoGWAS/gwas.py
@@ -1,6 +1,6 @@
 import os.path as op
 import sys
-from schnablelab.apps.base import SLURM_header, ActionDispatcher, OptionParser
+from schnablelab.apps.base import ActionDispatcher, OptionParser, create_slurm
 from .base import FarmCPU_header, GAPIT_header, MVP_Data_header, MVP_Run_header
 
 gemma = op.abspath(op.dirname(__file__)) + '/../apps/gemma'
@@ -11,9 +11,9 @@ tassel = op.abspath(op.dirname(__file__)) +\
 def main():
     actions = (
         ('farmcpu', 'run GWAS using FarmCPU (muti-loci mixed model)'),
-        ('gapit_cMLM', 'run GWAS using GAPIT compressed Mixed Linear Model'),
-        ('gemma_GLM', 'run GWAS using GEMMA general linear model'),
-        ('gemma_MLM', 'run GWAS using GEMMA mixed linear model '),
+        ('gapit_cMLM', 'run GWAS using GAPIT (compressed Mixed Linear Model)'),
+        ('gemma_GLM', 'run GWAS using GEMMA (general linear model)'),
+        ('gemma_MLM', 'run GWAS using GEMMA (mixed linear model)'),
         ('mvp_data', 'prepare files for running MVP'),
         ('mvp', 'Run both MLM and FarmCPU models in MVP'),
         )
@@ -23,11 +23,16 @@ def main():
 
 def farmcpu(args):
     """
-    %prog farmcpu phenotype.csv genotype_data_prefix PCA_file
+    %prog farmcpu phenotype_file genotype_data_prefix PCA_file
 
-    phenotype.csv with header, tab delimited
-    genotype_data_prefix for GM and GD files
-    Chr must be numbers in the GM file
+    Args:
+        phenotype_file:
+            phenotype file is tab delimited with header
+        genotype_data_prefix:
+            file prefix for GM and GD genotype files
+            Chr must be numbers in the GM file
+        PCA_file:
+            PCA file
     """
     p = OptionParser(farmcpu.__doc__)
     p.add_slurm_opts(job_prefix='farmcpu')
@@ -36,31 +41,34 @@ def farmcpu(args):
         sys.exit(not p.print_help())
 
     pheno, geno_prefix, PCA = args
-    mem = '.'.join(pheno.split('/')[-1].split('.')[0:-1])
-    with open(f'{mem}.FarmCPU.R', 'w') as f:
-        farmcpu_cmd = FarmCPU_header.format(pheno, geno_prefix, geno_prefix,
-                                            PCA, mem)
+    memo = '.'.join(pheno.split('/')[-1].split('.')[0:-1])
+    r_fn = f'{memo}.FarmCPU.R'
+    with open(r_fn, 'w') as f:
+        farmcpu_cmd = FarmCPU_header.format(pheno=pheno,
+                                            geno_prefix=geno_prefix,
+                                            pca=PCA,
+                                            memo=memo)
         f.write(farmcpu_cmd)
+    print(f'R file to run on premises: {r_fn}')
 
-    with open('{mem}.FarmCPU.slurm', 'w') as f:
-        h = SLURM_header
-        h += 'module load R/3.3\n'
-        header = h.format(opts.time, opts.memory, opts.prefix, opts.prefix,
-                          opts.prefix)
-        f.write(header)
-        cmd = 'R CMD BATCH %s.FarmCPU.R' % mem
-        f.write(cmd)
-    print(f'R script {mem}.FarmCPU.R and slurm file {mem}.FarmCPU.slurm has '
-          'been created. You can sbatch your job file now.')
+    cmd_header = 'module load R/3.3\n'
+    cmd = f'R CMD BATCH {r_fn}'
+    slurm_dict = vars(opts)
+    slurm_dict['cmd_header'] = cmd_header
+    create_slurm([cmd], slurm_dict)
 
 
 def gapit_cMLM(args):
     """
-    %prog gapit_cMLM phenotype.csv genotype_data_prefix PCA_file Kinship_file
+    %prog gapit_cMLM phenotype_file genotype_data_prefix PCA_file Kinship_file
 
     Run GAPIT compressed Mixed Linear Model
-    phenotype.csv is tab delimited with header
-    genotype_data_prefix for GM and GD files
+
+    Args:
+        phenotype_csv: phenotype file is tab delimited with header
+        genotype_data_prefix: prefix of GM and GD files
+        PCA_file: input PCA file
+        Kinship_file: input Kinship matrix file
     """
     p = OptionParser(gapit_cMLM.__doc__)
     p.add_slurm_opts(job_prefix='gapit_cMLM')
@@ -70,22 +78,22 @@ def gapit_cMLM(args):
         sys.exit(not p.print_help())
 
     pheno, geno_prefix, PCA, Kinship = args
-    mem = '.'.join(pheno.split('.')[0:-1])
-    with open('%s.cMLM.R' % mem, 'w') as f:
-        gapit_cmd = GAPIT_header.format(pheno, geno_prefix, geno_prefix, PCA,
-                                        Kinship, mem)
+    memo = '.'.join(pheno.split('.')[0:-1])
+    r_fn = f'{memo}.cMLM.R'
+    with open(r_fn, 'w') as f:
+        gapit_cmd = GAPIT_header.format(pheno=pheno,
+                                        geno_prefix=geno_prefix,
+                                        pca=PCA,
+                                        kinship=Kinship,
+                                        memo=memo)
         f.write(gapit_cmd)
+    print(f'R file to run on premises: {r_fn}')
 
-    with open('%s.cMLM.slurm' % mem, 'w') as f:
-        h = SLURM_header
-        h += 'module load R/3.3\n'
-        header = h.foramt(opts.time, opts.memory, opts.prefix, opts.prefix,
-                          opts.prefix)
-        f.write(header)
-        cmd = 'R CMD BATCH %s.cMLM.R\n' % mem
-        f.write(cmd)
-    print(f'R script {mem}.cMLM.R and slurm file {mem}.cMLM.slurm has been '
-          'created. You can sbatch your job file now.')
+    cmd_header = 'module load R/3.3\n'
+    cmd = f'R CMD BATCH {r_fn}\n'
+    slurm_dict = vars(opts)
+    slurm_dict['cmd_header'] = cmd_header
+    create_slurm([cmd], slurm_dict)
 
 
 def gemma_GLM(args):
@@ -93,7 +101,9 @@ def gemma_GLM(args):
     %prog gemma_GLM phenotype.csv genotype_data_prefix output_dir
 
     RUN GEMMA General Linear Model
-    genotype_data_prefix of GEMMA mean and annotation files
+    Args:
+        genotype_data_prefix:
+            filename prefix of GEMMA mean and annotation files
     """
     p = OptionParser(gemma_GLM.__doc__)
     p.add_slurm_opts(job_prefix='gemma_GLM')
@@ -104,18 +114,12 @@ def gemma_GLM(args):
     pheno, geno_prefix, output_dir = args
     meanG, annoG = geno_prefix+'.mean', geno_prefix+'.annotation'
     output_prefix = pheno.split('.')[0]
-    cmd = '%s -g %s -p %s -a %s -lm 4 -outdir %s -o %s'\
-        % (gemma, meanG, pheno, annoG, output_dir, output_prefix)
-    print('Command running on the local node:\n', cmd)
+    cmd = f'{gemma} -g {meanG} -p {pheno} -a {annoG} -lm 4 -outdir '\
+          f'{output_dir} -o {output_prefix}'
+    print(f'command to run on premises:\n\t{cmd}')
 
-    h = SLURM_header
-    header = h.format(opts.time, opts.memory, opts.prefix, opts.prefix,
-                      opts.prefix)
-    header += cmd
-    with open('%s.glm.slurm' % output_prefix, 'w') as f:
-        f.write(header)
-    print(f'slurm file {output_prefix}.glm.slurm has been created.'
-          'you can sbatch your job now.')
+    slurm_dict = vars(opts)
+    create_slurm([cmd], slurm_dict)
 
 
 def gemma_MLM(args):
@@ -123,7 +127,9 @@ def gemma_MLM(args):
     %prog gemma_MLM phenotype.csv genotype_data_prefix output_dir
 
     RUN GEMMA Mixed Linear Model
-    genotype_data_prefix of GEMMA mean and annotation files
+    Args:
+        genotype_data_prefix:
+            filename prefix of GEMMA mean and annotation files
     """
     p = OptionParser(gemma_MLM.__doc__)
     p.add_option('--kinship', default=False,
@@ -144,16 +150,10 @@ def gemma_MLM(args):
         cmd += ' -k %s' % opts.kinship
     if opts.pca:
         cmd += ' -c %s' % opts.pca
-    print('The command running on the local node:\n', cmd)
+    print('command to run on premises:\n', cmd)
 
-    h = SLURM_header
-    header = h.format(opts.time, opts.memory, opts.prefix, opts.prefix,
-                      opts.prefix)
-    header += cmd
-    with open('%s.mlm.slurm' % output_prefix, 'w') as f:
-        f.write(header)
-    print(f'slurm file {output_prefix}.mlm.slurm has been created.'
-          'you can sbatch your job file.')
+    slurm_dict = vars(opts)
+    create_slurm([cmd], slurm_dict)
 
 
 def mvp_data(args):
@@ -168,24 +168,25 @@ def mvp_data(args):
     if len(args) == 0:
         sys.exit(not p.print_help())
     hmp, output_prefix, = args
-    with open(f'{output_prefix}.mvp.data.R', 'w') as f:
-        f.write(MVP_Data_header % (hmp, op))
+    r_fn = f'{output_prefix}.mvp.data.R'
+    with open(r_fn, 'w') as f:
+        f.write(MVP_Data_header.format(hapmap=hmp,
+                                       output=output_prefix))
+    print(f'R file to run on premises: {r_fn}')
 
-    with open(f'{output_prefix}.mvp.data.slurm', 'w') as f:
-        header = SLURM_header.format(opts.time, opts.memory, output_prefix,
-                                     output_prefix, output_prefix)
-        header += 'module load R\n'
-        header += f'R CMD BATCH {output_prefix}.mvp.data.R\n'
-        f.write(header)
-    print(f'{output_prefix}.mvp.data.R and {output_prefix}.mvp.data.slurm '
-          'have been created. You can submit slurm job now.')
+    cmd_header = 'module load R\n'
+    cmd = f'R CMD BATCH {r_fn}\n'
+    slurm_dict = vars(opts)
+    slurm_dict['cmd_header'] = cmd_header
+    create_slurm([cmd], slurm_dict)
 
 
 def mvp(args):
     """
     %prog mvp phenotype.csv GMKC_prefix
 
-    GMKC_prefix: the prefix of Genotype_Map_Kinship_Covariates files.
+    Args:
+        GMKC_prefix: the prefix of Genotype_Map_Kinship_Covariates files.
     """
     p = OptionParser(mvp.__doc__)
     p.add_slurm_opts(job_prefix='mvp')
@@ -193,19 +194,17 @@ def mvp(args):
     if len(args) == 0:
         sys.exit(not p.print_help())
     pheno, output_prefix, = args
-    with open(f'{output_prefix}.mlm.farmcpu.R', 'w') as f:
-        f.write(MVP_Run_header.format(pheno, output_prefix, output_prefix,
-                                      output_prefix, output_prefix))
-    with open(f'{output_prefix}.mlm.farmcpu.slurm', 'w') as f:
-        header = SLURM_header.format(opts.time, opts.memory, output_prefix,
-                                     output_prefix, output_prefix)
-        header += 'module load R\n'
-        header += f'R CMD BATCH {output_prefix}.mlm.farmcpu.R\n' % opts.prefix
-        f.write(header)
+    r_fn = f'{output_prefix}.mlm.farmcpu.R'
+    with open(r_fn, 'w') as f:
+        f.write(MVP_Run_header.format(pheno=pheno,
+                                      output_prefix=output_prefix))
+    print(f'R file to run on premises: {r_fn}')
 
-    print(f'{output_prefix}.mlm.farmcpu.R and '
-          f'{output_prefix}.mlm.farmcpu.slurm have been created.'
-          'You can submit slurm job now.')
+    cmd_header = 'module load R\n'
+    cmd = f'R CMD BATCH {r_fn}\n'
+    slurm_dict = vars(opts)
+    slurm_dict['cmd_header'] = cmd_header
+    create_slurm([cmd], slurm_dict)
 
 
 if __name__ == "__main__":
